aeeddd9ddf800a49f0026cc02aa5cbba8735f879
toannq0603 <toannq06031999@gmail.com>
2019-06-23 23:47:11 +0700

    s

--- deletedata.php
+++ deletedata.php
@@ -55,7 +55,7 @@ body {font-size:16px;}
 <!-- Delete -->
   <div class="w3-container" id="contact" style="margin-top:75px">
     <h1 class="w3-xxxlarge w3-text-red"><b></b></h1>
-    <hr style="width:50px>
+    <hr style="width:50px;border:5px solid red" class="w3-round">
     <p>Do you want to delete some function!</p>
     <form name="DeleteData" action="deletedata.php" method="POST">
       <div class="w3-section">
